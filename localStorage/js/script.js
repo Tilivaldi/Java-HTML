4269987b23a0464b34cf814d0bac72f4c63c597f
unknown <tilvaldi@mail.ru>
2024-01-13 20:46:59 +0600

    Local in stydy

--- localStorage/js/script.js
+++ localStorage/js/script.js
@@ -1,32 +1,61 @@
-let users = []; 
-// localStorage.clear();
-    function addUser(){
-      const formElement = document.getElementById('form1');
-      var forms = document.forms;
-      
-      let name = document.getElementById('name');
-      let surname = document.getElementById('surname');
-      let country = document.getElementById('country');
-      let phone = document.getElementById('phone');
-
- 
-
-      let newUser = {};
-
-      newUser.name = name;
-      newUser.surname = surname;
-      newUser.country = country;
-      newUser.phone = parseInt(phone); 
-
-      users.push(newUser); 
-
-      updateStorage(); //обновляем localStorage после изменения массива
-
-      updateUserList();
-      function updateStorage(){
-
-        localStorage.users = JSON.stringify(users);
+let usersArray = [];
+let name ="";
+  let surname="";
+  let country="";
+  let phone="";
   
-      }
+// localStorage.clear(); при загрузке нполнение из локалсторадж
 
-    }
+
+function addUser() {
+
+  name = document.getElementById("name").value;
+  surname = document.getElementById('surname').value;
+  country = document.getElementById('country').value;
+  phone = document.getElementById('phone').value;
+  
+  let newUser = {};
+  newUser.name = name;
+  newUser.surname = surname;
+  newUser.country = country;
+  newUser.phone = phone;
+
+  usersArray.push(newUser);
+
+  updateStorage();
+}
+
+function updateStorage() {
+  localStorage.form = JSON.stringify(usersArray);
+}
+
+
+function initArray(){
+  if(localStorage.form != null){
+    usersArray = JSON.parse(localStorage.form);
+  }
+}
+
+function saveToDraft(){
+  let name = document.getElementById("name").value;
+  let surname = document.getElementById('surname').value;
+  let country = document.getElementById('country').value;
+  let phone = document.getElementById('phone').value;
+  
+  let newUser = {};
+  newUser.name = name;
+  newUser.surname = surname;
+  newUser.country = country;
+  newUser.phone = phone;
+
+  usersArray.push(newUser);
+
+  updateStorage();
+
+  
+}
+
+if (localStorage.getItem(form)!=null){
+  let userDraft = JSON.parse(localStorage.getItem(form));
+  document.getElementById("name").value = userDraft.name;
+}
