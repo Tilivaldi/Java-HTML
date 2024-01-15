380fb9babb70501dc2f44abbd29fb54a0247ae9f
Tilivaldi Amrayev <tilvaldi@mail.ru>
2024-01-16 00:35:55 +0600

    firstLocal

--- localStorage/js/script.js
+++ localStorage/js/script.js
@@ -1,27 +1,58 @@
 let usersArray = [];
-let name ="";
-  let surname="";
-  let country="";
-  let phone="";
-  
+let id=0;
+let SaveToDraft = 0;
+let name = "";
+let surname = "";
+let country = "";
+let phone = "";
 // localStorage.clear(); при загрузке нполнение из локалсторадж
 
+function initArray() {
+  if (localStorage.form != null) {
+    usersArray = JSON.parse(localStorage.form);
+  }
+  console.log(SaveToDraft);
+  let userNewArray = JSON.parse(localStorage.form)
+  let count = 0;
+  for (let key in userNewArray) {
+    count++
+  }  
+  
+  document.getElementById("name").value = userNewArray[count-1].name;
+  document.getElementById("surname").value = userNewArray[count-1].surname;
+  document.getElementById("country").value = userNewArray[count-1].country;
+  document.getElementById("phone").value = userNewArray[count-1].phone;
+  // if (SaveToDraft===1){
+  //   let userNewArray = JSON.parse(localStorage.form)
+  //   let count = 0;
+  //   for (let key in userNewArray) {
+  //     count++
+  //   }  
+    
+  //   document.getElementById("name").value = userNewArray[count-1].name;
+  //   document.getElementById("surname").value = userNewArray[count-1].surname;
+  //   document.getElementById("country").value = userNewArray[count-1].country;
+  //   document.getElementById("phone").value = userNewArray[count-1].phone;
+  // }
+  
+}
 
 function addUser() {
 
+  
   name = document.getElementById("name").value;
   surname = document.getElementById('surname').value;
   country = document.getElementById('country').value;
   phone = document.getElementById('phone').value;
-  
+
   let newUser = {};
   newUser.name = name;
   newUser.surname = surname;
   newUser.country = country;
   newUser.phone = phone;
-
+  newUser.id=id;
   usersArray.push(newUser);
-
+  id++;
   updateStorage();
 }
 
@@ -30,32 +61,34 @@ function updateStorage() {
 }
 
 
-function initArray(){
-  if(localStorage.form != null){
-    usersArray = JSON.parse(localStorage.form);
-  }
-}
 
-function saveToDraft(){
-  let name = document.getElementById("name").value;
-  let surname = document.getElementById('surname').value;
-  let country = document.getElementById('country').value;
-  let phone = document.getElementById('phone').value;
+function saveToDraft() {
+
   
+  name = document.getElementById("name").value;
+  surname = document.getElementById('surname').value;
+  country = document.getElementById('country').value;
+  phone = document.getElementById('phone').value;
+
   let newUser = {};
   newUser.name = name;
   newUser.surname = surname;
   newUser.country = country;
   newUser.phone = phone;
-
+  newUser.id=id;
   usersArray.push(newUser);
-
+  id++;
+  SaveToDraft = 1;
   updateStorage();
 
-  
+
 }
 
-if (localStorage.getItem(form)!=null){
-  let userDraft = JSON.parse(localStorage.getItem(form));
-  document.getElementById("name").value = userDraft.name;
-}
+
+
+
+
+
+
+
+
--- localStorage/js/script2.js
+++ localStorage/js/script2.js
@@ -1,5 +1,27 @@
-document.forms.publish.onsubmit = function() {
-    var message = this.message.value;
-    console.log(message)
-    return false;
-  };
+const form = document.querySelector('form')
+let usersArray = localStorage.getItem('users') ? JSON.parse(localStorage.getItem('users')) : []
+localStorage.setItem('users', JSON.stringify(usersArray))
+const data = JSON.parse(localStorage.getItem('users'))
+const liMaker = text => {
+  const li = document.createElement('li')
+  li.textContent = text
+  ul.appendChild(li)
+}
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault()
+  usersArray.push(input.value)
+  localStorage.setItem('users', JSON.stringify(usersArray))
+  liMaker(input.value)
+  input.value = ''
+})
+data.forEach(item => {
+  liMaker(item)
+})
+
+function addUser(){
+  localStorage.clear()
+  while (ul.firstChild) {
+    ul.removeChild(ul.firstChild)
+  }
+}
